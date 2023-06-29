c5b4adf9bd3b161539da981b729c3d2069695d2e
alexa <alexa@ROG-1>
2023-06-29 14:02:56 +0200

    Add project files.

---
+++ timezone-offset/MarketsModel.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+
+namespace timezone_offset;
+
+public class MarketsModel
+{
+    [JsonProperty("markets")]
+    public List<Market> Markets { get; set; }
+
+    public MarketsModel()
+    {
+        Markets = new List<Market>();
+    }
+}
+
+public class Market
+{
+    [JsonProperty("domain")]
+    public string Domain { get; set; }
+
+    [JsonProperty("locale")]
+    public string Locale { get; set; }
+
+    [JsonProperty("timeZone")]
+    public string TimeZone { get; set; }
+
+    [JsonProperty("marketCode")]
+    public string MarketCode { get; set; }
+
+    [JsonProperty("countryCode")]
+    public string CountryCode { get; set; }
+
+    [JsonProperty("countryName")]
+    public string CountryName { get; set; }
+
+    [JsonProperty("cultureCode")]
+    public string CultureCode { get; set; }
+
+    [JsonProperty("languageCode")]
+    public string LanguageCode { get; set; }
+
+    [JsonProperty("languageName")]
+    public string LanguageName { get; set; }
+
+    [JsonProperty("countryNameLocale")]
+    public string CountryNameLocale { get; set; }
+
+    [JsonProperty("urlLanguageFolder")]
+    public string UlLanguageFolder { get; set; }
+
+    [JsonProperty("available_products")]
+    public string[] AvailableProducts { get; set; }
+
+    [JsonProperty("secondaryCultureCode")]
+    public string SecondaryCultureCode { get; set; }
+
+    [JsonProperty("qaDomain")]
+    public string QaDomain { get; set; }
+
+    [JsonProperty("liveDomain")]
+    public string LiveDomain { get; set; }
+}
---
+++ timezone-offset/Program.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using NodaTime;
+using NodaTime.Text;
+
+namespace timezone_offset;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Parse the markets we have from the markets.json
+        // https://github.com/ILC-Technology/martech-toolkit/blob/main/libs/env-utils/src/lib/markets/markets.json
+        MarketsModel marketsData;
+        using (StreamReader file = File.OpenText(@"markets.json"))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            marketsData = (MarketsModel)serializer.Deserialize(file, objectType: typeof(MarketsModel))!;
+        }
+
+
+        string marketCode = "se";
+        Market? market = marketsData.Markets.FirstOrDefault(m => m.MarketCode == marketCode);
+
+        DateTime utcTime = ConvertToUtc("2023-06-29", "10:00:00", market.TimeZone);
+    }
+
+    public static DateTime ConvertToUtc(string dateString, string timeString, string timeZoneId)
+    {
+        try
+        {
+            // Parse the date and time strings
+            LocalDate date = LocalDatePattern.Iso.Parse(dateString).Value;
+            LocalTime time = LocalTimePattern.ExtendedIso.Parse(timeString).Value;
+
+            // Combine the date and time into a LocalDateTime
+            LocalDateTime localDateTime = date + time;
+
+            return ConvertToUtc(localDateTime, timeZoneId);
+        }
+        catch (Exception)
+        {
+            // If there is an error or the timezone is not found, you can handle it as per your requirements
+            throw new ArgumentException("Bad date/time or timezone");
+        }
+    }
+
+    public static DateTime ConvertToUtc(LocalDateTime localDateTime, string timeZoneId)
+    {
+        try
+        {
+            // Load the NodaTime timezone provider
+            var tzdb = DateTimeZoneProviders.Tzdb;
+
+            // Get the NodaTime timezone for the provided timezone ID
+            var timeZone = tzdb.GetZoneOrNull(timeZoneId);
+
+            if (timeZone != null)
+            {
+
+                // Create a ZonedDateTime object by combining the LocalDateTime and the TimeZone
+                var zonedDateTime = localDateTime.InZoneStrictly(timeZone);
+
+                // Convert the ZonedDateTime to an Instant representing the corresponding UTC time
+                var instant = zonedDateTime.ToInstant();
+
+                // Convert the Instant to a DateTime in UTC
+                var utcDateTime = instant.ToDateTimeUtc();
+
+                return utcDateTime;
+            }
+        }
+        catch (Exception)
+        {
+            // If there is an error or the timezone is not found, you can handle it as per your requirements
+        }
+
+        // If the timezone ID is not found, return the original DateTime value
+        return localDateTime.ToDateTimeUnspecified();
+    }
+
+}
